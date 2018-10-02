128a70385156ac8426f7cb09594520229621f3bb
caslei <lei.wang.sibet@gmail.com>
2018-10-02 15:52:36 -0400

    Linear Elasticity Value

--- niftyreg/reg-test/matlab_tests/getLinearElasticityValue_test.m
+++ niftyreg/reg-test/matlab_tests/getLinearElasticityValue_test.m
@@ -43,8 +43,7 @@ for i=1:2
                 jacobian = jacobian - eye(2);
                 for a=1:2
                     for b=1:2
-                        constraint_approx = constraint_approx + ...
-                            (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
+                        constraint_approx = constraint_approx + (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
                     end
                 end
             else
@@ -53,35 +52,17 @@ for i=1:2
                     for a=1:3
                         for b=1:3
                             for c=1:3
-                                jacobian(1,1)=jacobian(1,1) +  ...
-                                    first(a) * basis(b) * basis(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 1);
-                                jacobian(1,2)=jacobian(1,2) +  ...
-                                    basis(a) * first(b) * basis(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 1);
-                                jacobian(1,3)=jacobian(1,3) +  ...
-                                    basis(a) * basis(b) * first(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 1);
+                                jacobian(1,1)=jacobian(1,1) + first(a) * basis(b) * basis(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 1);
+                                jacobian(1,2)=jacobian(1,2) + basis(a) * first(b) * basis(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 1);
+                                jacobian(1,3)=jacobian(1,3) + basis(a) * basis(b) * first(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 1);
                                 
-                                jacobian(2,1)=jacobian(2,1) +  ...
-                                    first(a) * basis(b) * basis(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 2);
-                                jacobian(2,2)=jacobian(2,2) +  ...
-                                    basis(a) * first(b) * basis(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 2);
-                                jacobian(2,3)=jacobian(2,3) +  ...
-                                    basis(a) * basis(b) * first(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 2);
+                                jacobian(2,1)=jacobian(2,1) + first(a) * basis(b) * basis(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 2);
+                                jacobian(2,2)=jacobian(2,2) + basis(a) * first(b) * basis(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 2);
+                                jacobian(2,3)=jacobian(2,3) + basis(a) * basis(b) * first(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 2);
                                 
-                                jacobian(3,1)=jacobian(3,1) +  ...
-                                    first(a) * basis(b) * basis(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 3);
-                                jacobian(3,2)=jacobian(3,2) +  ...
-                                    basis(a) * first(b) * basis(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 3);
-                                jacobian(3,3)=jacobian(3,3) +  ...
-                                    basis(a) * basis(b) * first(c) * ...
-                                    grid_data(x+a-2, y+b-2, z+c-2, 1, 3);
+                                jacobian(3,1)=jacobian(3,1) + first(a) * basis(b) * basis(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 3);
+                                jacobian(3,2)=jacobian(3,2) + basis(a) * first(b) * basis(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 3);
+                                jacobian(3,3)=jacobian(3,3) + basis(a) * basis(b) * first(c) * grid_data(x+a-2, y+b-2, z+c-2, 1, 3);
                             end
                         end
                     end
@@ -91,25 +72,19 @@ for i=1:2
                     jacobian = jacobian - eye(3);
                     for a=1:3
                         for b=1:3
-                            constraint_approx = constraint_approx + ...
-                                (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
+                            constraint_approx = constraint_approx + (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
                         end
                     end
                 end
             end
         end
     end
-    dlmwrite([output_path,'/le_spline_approx',num2str(i+1),'D.txt'], ...
-        constraint_approx/ numel(grid_data), ...
-        'precision','%.6f','delimiter',' ');
+    dlmwrite([output_path,'/le_spline_approx',num2str(i+1),'D.txt'], constraint_approx/ numel(grid_data), 'precision','%.6f','delimiter',' ');
     
     %Read the deformation field image
     def_image = load_untouch_nii(defField_name{i});
     def_data = def_image.img;
-    def_dim=[def_image.hdr.dime.dim(2), ...
-        def_image.hdr.dime.dim(3), ...
-        def_image.hdr.dime.dim(4) ...
-        ];
+    def_dim=[def_image.hdr.dime.dim(2), def_image.hdr.dime.dim(3), def_image.hdr.dime.dim(4)];
     spacing = grid_image.hdr.dime.pixdim(2) / def_image.hdr.dime.pixdim(2);
     constraint_dense = 0;
     
@@ -119,27 +94,21 @@ for i=1:2
         norm_x = x/spacing - pre_x;
         basis_x = getBSplineCoefficient(norm_x);
         first_x = getBSplineCoefficientFirstOrder(norm_x);
+	
         for y=0:def_dim(2)-1
             pre_y = floor(y/spacing);
             norm_y = y/spacing - pre_y;
             basis_y = getBSplineCoefficient(norm_y);
             first_y = getBSplineCoefficientFirstOrder(norm_y);
+
             if (i+1)==2
                 jacobian = zeros(2,2);
                 for a=1:4
                     for b=1:4
-                        jacobian(1,1)=jacobian(1,1) + ...
-                            first_x(a) * basis_y(b) * ...
-                            grid_data(pre_x+a, pre_y+b, 1, 1, 1);
-                        jacobian(1,2)=jacobian(1,2) + ...
-                            basis_x(a) * first_y(b) * ...
-                            grid_data(pre_x+a, pre_y+b, 1, 1, 1);
-                        jacobian(2,1)=jacobian(2,1) + ...
-                            first_x(a) * basis_y(b) * ...
-                            grid_data(pre_x+a, pre_y+b, 1, 1, 2);
-                        jacobian(2,2)=jacobian(2,2) + ...
-                            basis_x(a) * first_y(b) * ...
-                            grid_data(pre_x+a, pre_y+b, 1, 1, 2);
+                        jacobian(1,1)=jacobian(1,1) + first_x(a) * basis_y(b) * grid_data(pre_x+a, pre_y+b, 1, 1, 1);
+                        jacobian(1,2)=jacobian(1,2) + basis_x(a) * first_y(b) * grid_data(pre_x+a, pre_y+b, 1, 1, 1);
+                        jacobian(2,1)=jacobian(2,1) + first_x(a) * basis_y(b) * grid_data(pre_x+a, pre_y+b, 1, 1, 2);
+                        jacobian(2,2)=jacobian(2,2) + basis_x(a) * first_y(b) * grid_data(pre_x+a, pre_y+b, 1, 1, 2);
                     end
                 end
                 jacobian = orientation(1:2,1:2) * jacobian';
@@ -148,8 +117,7 @@ for i=1:2
                 jacobian = jacobian - eye(2);
                 for a=1:2
                     for b=1:2
-                        constraint_dense = constraint_dense + ...
-                            (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
+                        constraint_dense = constraint_dense + (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
                     end
                 end
             else
@@ -158,39 +126,22 @@ for i=1:2
                     norm_z = z/spacing - pre_z;
                     basis_z = getBSplineCoefficient(norm_z);
                     first_z = getBSplineCoefficientFirstOrder(norm_z);
+
                     jacobian = zeros(3,3);
                     for a=1:4
                         for b=1:4
                             for c=1:4
-                                jacobian(1,1)=jacobian(1,1) +  ...
-                                    first_x(a) * basis_y(b) * basis_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 1);
-                                jacobian(1,2)=jacobian(1,2) +  ...
-                                    basis_x(a) * first_y(b) * basis_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 1);
-                                jacobian(1,3)=jacobian(1,3) +  ...
-                                    basis_x(a) * basis_y(b) * first_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 1);
+                                jacobian(1,1)=jacobian(1,1) + first_x(a) * basis_y(b) * basis_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 1);
+                                jacobian(1,2)=jacobian(1,2) + basis_x(a) * first_y(b) * basis_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 1);
+                                jacobian(1,3)=jacobian(1,3) + basis_x(a) * basis_y(b) * first_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 1);
                                 
-                                jacobian(2,1)=jacobian(2,1) +  ...
-                                    first_x(a) * basis_y(b) * basis_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 2);
-                                jacobian(2,2)=jacobian(2,2) +  ...
-                                    basis_x(a) * first_y(b) * basis_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 2);
-                                jacobian(2,3)=jacobian(2,3) +  ...
-                                    basis_x(a) * basis_y(b) * first_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 2);
+                                jacobian(2,1)=jacobian(2,1) + first_x(a) * basis_y(b) * basis_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 2);
+                                jacobian(2,2)=jacobian(2,2) + basis_x(a) * first_y(b) * basis_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 2);
+                                jacobian(2,3)=jacobian(2,3) + basis_x(a) * basis_y(b) * first_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 2);
                                 
-                                jacobian(3,1)=jacobian(3,1) +  ...
-                                    first_x(a) * basis_y(b) * basis_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 3);
-                                jacobian(3,2)=jacobian(3,2) +  ...
-                                    basis_x(a) * first_y(b) * basis_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 3);
-                                jacobian(3,3)=jacobian(3,3) +  ...
-                                    basis_x(a) * basis_y(b) * first_z(c) * ...
-                                    grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 3);
+                                jacobian(3,1)=jacobian(3,1) + first_x(a) * basis_y(b) * basis_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 3);
+                                jacobian(3,2)=jacobian(3,2) + basis_x(a) * first_y(b) * basis_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 3);
+                                jacobian(3,3)=jacobian(3,3) + basis_x(a) * basis_y(b) * first_z(c) * grid_data(pre_x+a, pre_y+b, pre_z+c, 1, 3);
                             end
                         end
                     end
@@ -200,8 +151,7 @@ for i=1:2
                     jacobian = jacobian - eye(3);
                     for a=1:3
                         for b=1:3
-                            constraint_dense = constraint_dense + ...
-                                (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
+                            constraint_dense = constraint_dense + (0.5*(jacobian(a,b)+jacobian(b,a)))^2;
                         end
                     end
                 end
@@ -341,6 +291,7 @@ basis(3) = (-3*dist*dist*dist + 3*dist*dist + 3*dist + 1)/6;
 basis(4) = dist*dist*dist/6;
 
 function first = getBSplineCoefficientFirstOrder(dist)
+%% dy/dx ==> first order derivative 
 %% Given a normalise position return the 4 corresponding basis values
 first(4)= dist * dist / 2;
 first(1)= dist - 0.5 - first(4);
