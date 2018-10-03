6c5bf967cfcc5147db177d059955946a0a3d4c0c
caslei <lei.wang.sibet@gmail.com>
2018-10-02 22:44:10 -0400

    matrix operation in matlab

--- niftyreg/reg-test/matlab_tests/matrix_operation_test.m
+++ niftyreg/reg-test/matlab_tests/matrix_operation_test.m
@@ -4,23 +4,27 @@ function matrix_operation_test(output_path)
 %%
 m=4;
 M1 = rand(m,m,'single')+ eye(4,4,'single');
-M1(end,:)=[0 0 0 1];
+M1(end,:)=[0 0 0 1];  % the last row
 M2 = rand(m,m,'single')+ eye(4,4,'single');
+
 dlmwrite([output_path,'/inputMatrix1.txt'],M1,'precision','%.6f','delimiter',' ');
 dlmwrite([output_path,'/inputMatrix2.txt'],M2,'precision','%.6f','delimiter',' ');
+
 %% READ THE FILES THAT WE HAVE JUST WRITTEN
 fileID = fopen([output_path,'/inputMatrix1.txt'],'r');
 formatSpec = '%f';
 sizeM1 = [m m];
 M1 = fscanf(fileID,formatSpec,sizeM1);
 fclose(fileID);
-M1=single(M1');
+M1=single(M1');% ======= why does it need to transpose matrix??? ========
+
 fileID2 = fopen([output_path,'/inputMatrix2.txt'],'r');
 formatSpec = '%f';
 sizeM2 = [m m];
 M2 = fscanf(fileID2,formatSpec,sizeM2);
 fclose(fileID2);
-M2=single(M2');
+M2=single(M2'); % ======= why does it need to transpose matrix??? ========
+
 %% 1. Multiplication
 expectedMul = single(double(M1)*double(M2));
 dlmwrite([output_path,'/expectedMulMatrix.txt'],expectedMul,'precision','%.6f','delimiter',' ');
@@ -31,11 +35,14 @@ dlmwrite([output_path,'/expectedAddMatrix.txt'],expectedAdd,'precision','%.6f','
 expectedSub = single(double(M1)-double(M2));
 dlmwrite([output_path,'/expectedSubMatrix.txt'],expectedSub,'precision','%.6f','delimiter',' ');
 %% 4. Exp
-expectedExp = single(expm(double(M1)));
+expectedExp = single(expm(double(M1)));  % === function 'expm' ===
 dlmwrite([output_path,'/expectedExpMatrix.txt'],expectedExp,'precision','%.6f','delimiter',' ');
 %% 5. Log
-expectedLog = single(logm(double(M1)));
+expectedLog = single(logm(double(M1)));  % === function 'logm' ===
 dlmwrite([output_path,'/expectedLogMatrix.txt'],expectedLog,'precision','%.6f','delimiter',' ');
 %% 6. Inv
 expectedInv = single(inv(double(M1)));
 dlmwrite([output_path,'/expectedInvMatrix.txt'],expectedInv,'precision','%.6f','delimiter',' ');
+
+
+
