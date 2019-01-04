d39c3cc1f6aa35401d792632d9706523bad4d740
Lei Wang <lei.wang.sibet@gmail.com>
2019-01-03 19:09:41 -0500

    run c codes in vim

---
+++ vim_run.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main()
+{
+	fprintf(stdout, "Hello, world!\n");
+	return 0;
+
+
+}
+
+// run commond: ":!gcc wlei.c"
+// 	     ":a.exe"
+// import SimpleITK as sitk
+// print("Hello, World!\n")
+// print("Wlei try to run python script")
+// run command: ":!python wlei.c"
+
+	     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
